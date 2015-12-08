94396e5d8bd0e6f7036d7fc72a6e791343482717
David Hasenjaeger <david@wirestorm.us>
2015-12-08 00:45:49 -0800

    styled home page and added social media links

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,19 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Home' });
+});
+
+router.get('/about', function(req, res, next) {
+  res.render('index', { title: 'about' });
+});
+
+router.get('/blog', function(req, res, next) {
+  res.render('index', { title: 'blog' });
+});
+
+router.get('/upcoming', function(req, res, next) {
+  res.render('index', { title: 'upcoming' });
 });
 
 module.exports = router;
